8f4c195097b76779d4efc5e2899834a3d0909b07
Shah <oracle.sh.2020@gmail.com>
2021-04-05 17:37:56 +0500

    add functions

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/Shah-2021/stats
+module github.com/Shah-2021/stats/v2
 
 go 1.16
 
-require github.com/Shah-2021/bank v0.0.0-20210331111552-04538396f780 // indirect
+require github.com/Shah-2021/bank/v2 v2.0.0
---
+++ pkg/stats/stats.go
@@ -0,0 +1,31 @@
+package stats
+
+import "github.com/Shah-2021/bank/v2/pkg/bank/types"
+
+// Avg рассчитывает среднюю сумму платежа.
+func Avg(payments []types.Payment) types.Money {
+	avgSum := types.Money(0)
+	ind := types.Money(0)
+	status := types.StatusFail
+	for _, v := range payments {
+		if v.Status != status  {
+		avgSum += v.Amount
+		ind ++
+		}
+	}
+	return types.Money(avgSum/ind)
+}
+
+
+// TotalInCategory находит сумму покупок в определённой категории.
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money{
+	totalSum := types.Money(0)
+	for _, v := range payments {
+		if category == v.Category {
+		if v.Status != types.StatusFail {
+	    	totalSum += v.Amount
+		}
+	}
+	}
+	return totalSum
+}
